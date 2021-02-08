ecc58fd1acf49120e704b45d40bd7e9563a72509
Suraj S <gssacharya@gmail.com>
2021-02-08 19:02:01 +0530

    ipv6 small update

--- ipv6tcpclient.py
+++ ipv6tcpclient.py
@@ -1,11 +1,9 @@
 import socket
 
-server_addr,server_port = "2409:4073:105:2beb:67cd:717a:ecab:73d5",8000
+server_addr,server_port = "",8000
 
 c = socket.socket(socket.AF_INET6,socket.SOCK_STREAM)
 c.connect((server_addr,server_port,0,0))
-
-c.recv(1024)
 
 m = c.recv(1024)
 c.send(b"received")
--- ipv6tcpserver.py
+++ ipv6tcpserver.py
@@ -1,6 +1,6 @@
 import socket
 
-bind_addr,bind_port = "2409:4073:105:2beb:67cd:717a:ecab:73d5",8000
+bind_addr,bind_port = "",8000
 
 s = socket.socket(socket.AF_INET6,socket.SOCK_STREAM)
 s.bind((bind_addr,bind_port,0,0))
@@ -8,7 +8,7 @@ s.bind((bind_addr,bind_port,0,0))
 s.listen(1)
 client,addr = s.accept()
 
-client.send(b"<html><body>connected</body></html>")
+client.send(b"server connected")
 m = client.recv(1024)
 
 print(m)
