6b883be63727d43aaf16b78290908ef689befeae
Sanjay <san1999.rnandanan@gmail.com>
2021-02-23 22:51:36 +0530

    Merge branch 'main' of https://github.com/JARUS-5/NITC-major-project into main

--- vid_client.py
+++ vid_client.py
@@ -1,6 +1,3 @@
-# Welcome to PyShine
-# lets make the client code
-# In this code client is sending video to server
 import socket,cv2, pickle,struct
 import pyshine as ps # pip install pyshine
 import imutils # pip install imutils
@@ -9,34 +6,27 @@ import time
 import select
 
 FORMAT = pyaudio.paInt16
-CHANNELS = 1
-RATE = 44100
-CHUNK = 4096
+CHANNELS = 2
+RATE = 192000
+CHUNK = 1024*16
 
 camera = True
 if camera == True:
     vid = cv2.VideoCapture(0)
-else:
-    vid = cv2.VideoCapture('videos/mario.mp4')
 client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 host_ip = socket.gethostbyname(socket.gethostname())#'192.168.1.11' # Here according to your server ip write the address
 port = 9999
 client_socket.connect((host_ip,port))
 audio_socket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 audio_socket.connect((host_ip,8080))
-read_list = [audio_socket]
 audio = pyaudio.PyAudio()
-
-def callback(in_data, frame_count, time_info, status):
-    for s in read_list[1:]:
-        s.sendall(in_data)
-    return (None, pyaudio.paContinue)
-stream = audio.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK, stream_callback=callback)
+stream = audio.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK)
 print ("recording...")
 if client_socket and audio_socket:
     while (vid.isOpened()):
         try:
-            stream.start_stream()
+            data=stream.read(CHUNK)
+            audio_socket.send(data)
             img, frame = vid.read()
             frame = imutils.resize(frame,width=380)
             a = pickle.dumps(frame)
