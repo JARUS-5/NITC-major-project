e62346cefb499361535c4bd7b12b8dea5b4682cd
Suraj S <suraj_b170389ec@nitc.ac.in>
2021-04-03 11:44:37 +0530

    before Gui v01

--- streamer.py
+++ streamer.py
@@ -3,10 +3,12 @@ import time
 import cv2
 import threading
 import numpy as np
+import json
 
 # get streamers's IP address
 #streamer_name = socket.gethostname()
 #streamer_IP = socket.gethostbyname(streamer_name)
+APP_STATE = True
 
 streamer_name = 'JARUS'
 streamer_IP = '127.0.0.1'
@@ -33,32 +35,25 @@ listener_IP_list = []
 
 # function to create the streamer "send list"
 def create_streamer_send_list(listener_IP_list):
+    global APP_STATE
     send_list = []
     i = 1
-    while True:
+    while APP_STATE:
         index = 2**i-2
         if index < len(listener_IP_list):
             send_list.append(listener_IP_list[index])
         else:
             break
         i += 1
-    print(send_list)
     return send_list
-
-udp_video_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-udp_video_socket.bind((streamer_IP, streamer_video_udp_port))
-
-webcam = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-
-buf = 0
 
 def video_streamer():
     global buf
-    frame = np.zeros((50,50,3))
+    frame = np.zeros((480,360,3))
     cv2.imshow("Streamer",frame)
     while (webcam.isOpened()) and (cv2.getWindowProperty("Streamer", cv2.WND_PROP_VISIBLE) >= 1):
         retval, frame = webcam.read()
-        frame = cv2.resize(frame, (50, 50))
+        frame = cv2.resize(frame, (480, 360))
         buf = cv2.imencode('.jpg', frame)[1]
         cv2.imshow("Streamer",frame)
         cv2.waitKey(33)
@@ -68,33 +63,49 @@ def video_streamer():
 def thread_vid_client(udp_video_socket,client_addr):
     global send_list
     global buf
-    while True:
+    global APP_STATE
+    while APP_STATE:
         try:
             if client_addr in send_list:
                 udp_video_socket.sendto(buf, (client_addr[0],client_addr[1]+2))
         except:
             break
 
-def thread_client_commander(tcp_socket):
+def thread_client_commander():
+    global listener_tcp_sockets
     global listener_IP_list
     global send_list
-    dl = []
-    ping_time = 100
-    for i in listener_IP_list:
-        dl.append({'ip':i,'ping':ping_time})
-    d = {'data':dl}
+    global APP_STATE
+    while APP_STATE:
+        dl = []
+        #ping_time = 100
+        #for i in listener_IP_list:
+        #    dl.append({"ip":i,"ping":ping_time})
+        #d = {"data":dl}
+        #print(d)
+        print(listener_IP_list)
+        for i in range(len(listener_tcp_sockets)-1,-1,-1):
+            try:
+                mssg = '{}:{}'.format(i,json.dumps(listener_IP_list))
+                listener_tcp_sockets[i].send(mssg.encode())
+            except:
+                del listener_IP_list[i]
+                del listener_tcp_sockets[i]
+                send_list = create_streamer_send_list(listener_IP_list)
+        time.sleep(5)
 
-    for i in range(len(listener_IP_list)):
-        try:
-            tcp_socket.sendall(str(d).encode(),listener_IP_list[i])
-        except:
-            del listener_IP_list[i]
-            send_list = create_streamer_send_list(listener_IP_list)
-    time.sleep(5)
+udp_video_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+udp_video_socket.bind((streamer_IP, streamer_video_udp_port))
+
+webcam = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+
+buf = 0
 
 thread_vid_client_ls = []
+listener_tcp_sockets = []
 
-thread_commnd_client = threading.Thread(target=thread_client_commander,args=(tcp_socket,))
+thread_commnd_client = threading.Thread(target=thread_client_commander)
+thread_commnd_client.start()
 video_thread = threading.Thread(target=video_streamer)
 video_thread.start()
 
@@ -118,6 +129,7 @@ while True:
 
     except KeyboardInterrupt:
         print("Streaming terminated")
+        APP_STATE = False
         break
 
 udp_video_socket.close()
--- threadlistener.py
+++ threadlistener.py
@@ -3,6 +3,7 @@ import time
 import cv2
 import threading
 import numpy as np
+import json
 
 # get listener's IP address
 #listener_name = socket.gethostname()
@@ -15,9 +16,9 @@ listener_IP = '127.0.0.1'
 streamer_tcp_port = 60000
 streamer_audio_udp_port = 60001
 streamer_video_udp_port = 60002
-listener_tcp_port = 60009
-listener_audio_udp_port = 60010
-listener_video_udp_port = 60011
+listener_tcp_port = 60003
+listener_audio_udp_port = 60004
+listener_video_udp_port = 60005
 
 streamer_IP = "127.0.0.1"
 streamer_tcp_port = 60000
@@ -32,25 +33,26 @@ tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 tcp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 tcp_socket.bind((listener_IP, listener_tcp_port))
 tcp_socket.connect((streamer_IP, streamer_tcp_port))
+tcp_socket.settimeout(20)
 
 def server_commands(tcp_socket):
     while True:
-        try:
-            tcp_socket.recv(1024)
-        except:
-            break
+        r = tcp_socket.recv(1024).decode().split(':')
+        d = json.loads(r[1])
+        print(d)
 
 udp_video_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 udp_video_socket.bind((listener_IP, listener_video_udp_port))
 udp_video_socket.settimeout(5)
 
 thread_server_commands = threading.Thread(target=server_commands,args=(tcp_socket,))
+thread_server_commands.start()
 
-frame = np.zeros((50,50,3))
+frame = np.zeros((480,360,3))
 cv2.imshow("Receiver",frame)
 while cv2.getWindowProperty("Receiver", cv2.WND_PROP_VISIBLE) >= 1:
     try:
-        buf = udp_video_socket.recv(5*1024)
+        buf = udp_video_socket.recv(650*1024)
         jpnp = np.frombuffer(buf, dtype = np.uint8)
         frame = cv2.imdecode(jpnp, cv2.IMREAD_COLOR)
         cv2.imshow("Receiver",frame)
